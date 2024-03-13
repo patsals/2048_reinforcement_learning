8b528b2d584e5e7d81e7c261e62770094455c8f4
Andrei <andreijsecor@gmail.com>
2024-03-12 17:40:01 -0700

    fixed bugs, adjusted font, rewrote with some numpy

--- 2048.py
+++ 2048.py
@@ -1,12 +1,27 @@
 import tkinter as tk
-import random
+import numpy as np
 # tutorial at: https://www.youtube.com/watch?v=b4XP2IcI-Bg
 
 class Game(tk.Frame):
+
     def __init__(self):
         tk.Frame.__init__(self)
         self.grid()
         self.master.title("2048")
+
+        self.NUM_COLORS = {
+            2: '#eee4da',
+            4: '#eee1c9',
+            8: '#f3b72a',
+            16: '#f69664',
+            32: '#f77c5f',
+            64: '#f75f3b',
+            128: '#edd073',
+            256: '#edcc62',
+            512: '#edc950',
+            1024: '#edc53f',
+            2048: '#edc22e'
+        }
 
         self.main_grid = tk.Frame(
             self, bg='grey', bd=3, width=600, height=600
@@ -22,6 +37,9 @@ class Game(tk.Frame):
         self.master.bind('<Down>', self.down)
 
         self.mainloop()
+    
+    def get_color(self, val):
+        return self.NUM_COLORS[val] if val in self.NUM_COLORS else 'tan'
 
     def make_GUI(self):
         self.cells = []
@@ -35,7 +53,7 @@ class Game(tk.Frame):
                     height=150
                 )
                 cell_frame.grid(row=i, column=j, padx=5, pady=5)
-                cell_number = tk.Label(self.main_grid, bg='white')
+                cell_number = tk.Label(self.main_grid, bg='white', font=("Arial bold", 40))
                 cell_number.grid(row=i, column=j)
                 cell_data = {'frame': cell_frame, 'number': cell_number}
                 row.append(cell_data)
@@ -49,30 +67,26 @@ class Game(tk.Frame):
             font='black'
         ).grid(row=0)
 
-        self.score_label = tk.Label(score_frame, text='0')
+        self.score_label = tk.Label(score_frame, text='0', font=("Arial", 12))
         self.score_label.grid(row=1)
 
     def start_game(self):
-        self.matrix = [[0]*4 for _ in range(4)]
+        self.matrix = np.zeros((4,4),dtype=int)
 
         # fill 2 random cells
-        row = random.randint(0,3)
-        col = random.randint(0,3)
-        self.matrix[row][col] = 2
-        self.cells[row][col]['frame'].configure(bg='tan')
-        self.cells[row][col]['number'].configure(
-            bg='tan',
+        ind = np.random.choice(range(16),2,replace=False)
+        row = ind//4
+        col = ind%4
+        self.matrix[row[0]][col[0]] = 2
+        self.matrix[row[1]][col[1]] = 2
+        self.cells[row[0]][col[0]]['frame'].configure(bg=self.get_color(2))
+        self.cells[row[0]][col[0]]['number'].configure(
+            bg=self.NUM_COLORS[2],
             text='2'
         )
-
-        while(self.matrix[row][col] != 0):
-            row = random.randint(0,3)
-            col = random.randint(0,3)
-
-        self.matrix[row][col] = 2
-        self.cells[row][col]['frame'].configure(bg='tan')
-        self.cells[row][col]['number'].configure(
-            bg='tan',
+        self.cells[row[1]][col[1]]['frame'].configure(bg=self.get_color(2))
+        self.cells[row[1]][col[1]]['number'].configure(
+            bg=self.NUM_COLORS[2],
             text='2'
         )
 
@@ -80,46 +94,47 @@ class Game(tk.Frame):
 
 
     def stack(self):
-        new_matrix = [[0]*4 for _ in range(4)]
+        ret = False
+        new_matrix = np.zeros((4,4),dtype=int)
         for i in range(4):
             fill_position = 0
             for j in range(4):
                 if self.matrix[i][j] != 0:
                     new_matrix[i][fill_position] = self.matrix[i][j]
                     fill_position += 1
-        self.matrix = new_matrix 
+                    ret = True
+        self.matrix = new_matrix
+        return ret
 
     def combine(self):
+        ret = False
+        # diff_mat = np.hstack((self.matrix[:,:3] - self.matrix[:,1:], np.ones((4,1),dtype=int)))
+        # self.matrix[diff_mat==0] *= 2
+        # self.matrix[np.roll(diff_mat,1,axis=1)==0] = 0
+        # self.score += sum(self.matrix[diff_mat==0])
         for i in range(4):
             for j in range(3):
                 if self.matrix[i][j] != 0 and self.matrix[i][j] == self.matrix[i][j+1]:
                     self.matrix[i][j] *= 2
                     self.matrix[i][j+1] = 0
                     self.score += self.matrix[i][j]
+                    ret = True
+        return ret
 
     def reverse(self):
-        new_matrix = []
-        for i in range(4):
-            new_matrix.append([])
-            for j in range(4):
-                new_matrix[i].append(self.matrix[i][3-j])
-        self.matrix = new_matrix
+        self.matrix = np.flip(self.matrix, axis=1)
 
     def transpose(self):
-        new_matrix = [[0]*4 for _ in range(4)]
-        for i in range(4):
-            for j in range(4):
-                new_matrix[i][j] = self.matrix[j][i]
-        self.matrix = new_matrix 
+        self.matrix = self.matrix.T
 
     def add_new_tile(self):
-        row = random.randint(0,3)
-        col = random.randint(0,3)
-        while(self.matrix[row][col] != 0):
-            row = random.randint(0,3)
-            col = random.randint(0,3)
+        ind = np.arange(16,dtype=int)[np.ndarray.flatten(self.matrix) == 0]
+        if (len(ind) > 0):
+            ind = np.random.choice(ind,1)[0]
+            row = ind//4
+            col = ind%4
+            self.matrix[row][col] = np.random.choice([2,4],p=[0.9,0.1])
 
-        self.matrix[row][col] = random.choice([2,4])
 
     def update_GUI(self):
         for i in range(4):
@@ -129,8 +144,9 @@ class Game(tk.Frame):
                     self.cells[i][j]['frame'].configure(bg='white')
                     self.cells[i][j]['number'].configure(bg='white', text='')
                 else:
-                    self.cells[i][j]['frame'].configure(bg='tan')
-                    self.cells[i][j]['number'].configure(bg='tan', text=str(cell_value))
+                    color = self.get_color(self.matrix[i][j])
+                    self.cells[i][j]['frame'].configure(bg=color)
+                    self.cells[i][j]['number'].configure(bg=color, text=str(cell_value))
 
         self.score_label.configure(text=self.score)
         self.update_idletasks()
@@ -178,28 +194,23 @@ class Game(tk.Frame):
 
     # check if any moves are possible
     def horizontal_move_exists(self):
-        for i in range(4):
-            for j in range(3):
-                if self.matrix[i][j] == self.matrix[i][j+1]:
-                    return True
-        return False
+        diff_mat = self.matrix[:,:3] - self.matrix[:,1:]
+        return np.any(diff_mat==0)
+    
     def vertical_move_exists(self):
-        for i in range(4):
-            for j in range(3):
-                if self.matrix[i][j] == self.matrix[i+1][j]:
-                    return True
-        return False
+        diff_mat = self.matrix[:3,:] - self.matrix[1:,:]
+        return np.any(diff_mat==0)
     
     # check if game is over win/lose
     def game_over(self):
-        if any(2048 in row for row in self.matrix):
+        if np.any(self.matrix==2048):
             game_over_frame = tk.Frame(self.main_grid, borderwidth=2)
             game_over_frame.place(relx=0.5, rely=0.5, anchor='center')
             tk.Label(
                 game_over_frame,
                 text='you win!'
             ).pack()
-        elif not any(0 in row for row in self.matrix) and not self.vertical_move_exists() and not self.horizontal_move_exists():
+        elif not np.any(self.matrix==0) and not self.vertical_move_exists() and not self.horizontal_move_exists():
             game_over_frame = tk.Frame(self.main_grid, borderwidth=2)
             game_over_frame.place(relx=0.5, rely=0.5, anchor='center')
             tk.Label(
