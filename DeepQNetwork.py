more bug fixes, normalization of action weights

DeepQNetwork.py
@@ -23,9 +23,9 @@ class DeepQNetwork(nn.Module):
     def forward(self, state):
         x = F.relu(self.fc1(state))
         x = F.relu(self.fc2(x))
-        actions = F.softmax(self.fc3(x))
+        actions = self.fc3(x)
 
-        return actions 
+        return actions
     
 
 class Agent():
@@ -68,8 +68,11 @@ class Agent():
     def choose_action(self, observation):
         if np.random.random() > self.epsilon:
             state = torch.tensor([observation]).to(self.device)
-            actions = self.Q_eval.forward(state)
-            action = torch.argmax(actions).item()
+            with torch.no_grad(): actions = self.Q_eval.forward(state)
+            amin = torch.min(actions).item()
+            amax = torch.max(actions).item()
+            p = (actions - amin)/(amax - amin)
+            action = torch.arange(actions.size(dim=-1)).to(self.device)[p.multinomial(num_samples=1).item()].item()
         else:
             action = np.random.choice(self.action_space)
 
@@ -90,10 +93,11 @@ class Agent():
         new_state_batch = torch.tensor(self.new_state_memory[batch]).to(self.device)
         reward_batch = torch.tensor(self.reward_memory[batch]).to(self.device)
         terminal_batch = torch.tensor(self.terminal_memory[batch]).to(self.device)
-
+        
         action_batch = self.action_memory[batch]
         q_eval = self.Q_eval.forward(state_batch)[batch_index, action_batch]
         q_next = self.Q_eval.forward(new_state_batch)
+        q_next = torch.clone(q_next)
         q_next[terminal_batch] = 0.0
         q_target = reward_batch + self.gamma * torch.max(q_next, dim=1)[0]
 

RL_game_2048.py
@@ -46,10 +46,18 @@ class Game(tk.Frame):
 
         self.show_plot = True
         self.scores = []
+        self.rewards = []
         self.highest_tiles = []
+
+        self.num_moves = 0
+        self.prev_highest_tile = 2
+        self.ep_reward = 0
+        self.plot_flag = False
 
         self.make_GUI()
         self.reset()
+
+        self.plot_flag = True
 
 
     # def get_observation(self):
@@ -82,6 +90,9 @@ class Game(tk.Frame):
         elif self.score != 0:
             self.scores.append(self.score)
             self.highest_tiles.append(max(self.state))
+        if (self.plot_flag):
+            self.rewards.append(self.ep_reward)
+        self.ep_reward = 0
         
         self.score = 0
         self.prev_score = 0
@@ -111,11 +122,14 @@ class Game(tk.Frame):
         reward = 0
 
         highest_tile = np.max(self.matrix)
-        if highest_tile not in [self.matrix[0][0], self.matrix[0][3], self.matrix[3][0], self.matrix[0][3]]:
-            reward -= 100
-        else:
-            #reward += self.score - self.prev_score
-            reward += 100
+        reward += (self.num_moves-6)*2
+        if (highest_tile > self.prev_highest_tile):
+            reward += highest_tile
+
+        self.num_moves = 0
+        self.prev_highest_tile = highest_tile
+
+        self.ep_reward += reward
 
         return np.array(self.state, dtype=np.float32), reward, terminated, self.score
 
@@ -153,7 +167,7 @@ class Game(tk.Frame):
         self.score_label.grid(row=1)
 
         if self.show_plot:
-            self.fig, self.ax = plt.subplots(2, figsize=(3, 4))
+            self.fig, self.ax = plt.subplots(3, figsize=(3, 5))
             self.canvas = FigureCanvasTkAgg(self.fig, master=self)
             self.canvas_widget = self.canvas.get_tk_widget()
             self.canvas_widget.grid(row=0, column=5, rowspan=4, padx=20, pady=20)
@@ -169,15 +183,22 @@ class Game(tk.Frame):
         #self.ax[0].set_xlabel('episode', fontsize=5)
         self.ax[0].set_ylabel('game score', fontsize=5)
         self.ax[0].tick_params(axis='both', labelsize=3)
+        
+        self.ax[1].clear()
+        self.ax[1].plot(self.rewards, color='red')
+        self.ax[1].set_title('Model reward over episodes', fontsize=6)
+        self.ax[1].set_ylabel('reward', fontsize=5)
+        self.ax[1].tick_params(axis='both', labelsize=3)
+
         self.canvas.draw()
 
     def update_histogram_plot(self):
-        self.ax[1].clear()
-        self.ax[1].hist(self.highest_tiles, color='blue')
-        self.ax[1].set_title('Highest Tile Distribution', fontsize=6)
-        #self.ax[1].set_xlabel('Score', fontsize=5)
-        self.ax[1].set_ylabel('Count', fontsize=5)
-        self.ax[1].tick_params(axis='both', labelsize=3)
+        self.ax[2].clear()
+        self.ax[2].hist(self.highest_tiles, color='blue')
+        self.ax[2].set_title('Highest Tile Distribution', fontsize=6)
+        #self.ax[2].set_xlabel('Score', fontsize=5)
+        self.ax[2].set_ylabel('Count', fontsize=5)
+        self.ax[2].tick_params(axis='both', labelsize=3)
         self.canvas.draw()
 
 
@@ -192,6 +213,7 @@ class Game(tk.Frame):
                     new_matrix[i][fill_position] = self.matrix[i][j]
                     fill_position += 1
                     ret = True
+                    self.num_moves += 1
         self.matrix = new_matrix
         self.state = self.matrix.flatten()
         return ret
@@ -210,6 +232,7 @@ class Game(tk.Frame):
                     self.matrix[i][j+1] = 0
                     self.score += self.matrix[i][j]
                     ret = True
+                    self.num_moves += 1
         self.state = self.matrix.flatten()
         return ret
 

main.py
@@ -31,7 +31,7 @@ if __name__ == '__main__':
     agent = Agent(gamma=0.99, epsilon=1.0, batch_size=64, n_actions=4,
                 eps_end=0.01, eps_dec=0.01, input_dims=[16], lr=0.03, device=torch.device('cuda:0' if torch.cuda.is_available() else 'cpu'))
 
-    n_games = 500
+    n_games = 5000
     game = Game()
 
     train_thread = Thread(target=lambda : train(game))